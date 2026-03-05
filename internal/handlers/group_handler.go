0930b362fd38f11adf1085ba4541133689be9af8
sjarso <stephenjacob815@gmail.com>
2026-03-05 16:46:22 +0300

    feat:Added handlers for group to create group and list all groups

---
+++ internal/handlers/group_handler.go
@@ -0,0 +1,47 @@
+package handlers
+
+import (
+	"database/sql"
+	"html/template"
+	"jumuia/internal/models"
+	"jumuia/internal/repository"
+	"net/http"
+)
+func CreateGroupHandler(db *sql.DB)http.HandlerFunc{
+	return func(w http.ResponseWriter,r *http.Request){
+		if r.Method != http.MethodPost{
+			http.Redirect(w,r,"/groups/new",http.StatusSeeOther)
+		}
+		name:=r.FormValue("name")
+		village := r.FormValue("village")
+		district :=r.FormValue("district")
+		 group:=models.Group{
+			Name:name,
+			Village: village,
+			District: district,
+		 }
+		 _,err:=repository.CreateGroup(db,group)
+		 if err != nil{
+			http.Error(w,"Error saving group",500)
+		 }
+		 http.Redirect(w,r,"/groups",http.StatusSeeOther)
+	}
+}
+// List all groups
+func ListGroupsHandler(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		groups, err := repository.GetAllGroups(db)
+		if err != nil {
+			http.Error(w, "Error fetching groups", 500)
+			return
+		}
+
+		tmpl, err := template.ParseFiles("web/templates/groups_list.html")
+		if err != nil {
+			http.Error(w, "Error loading template", 500)
+			return
+		}
+
+		tmpl.Execute(w, groups)
+	}
+}
